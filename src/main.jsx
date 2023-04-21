2cea76075d668365deeebab8f18a7020a8ae7085
Sergey <sergsmile29@gmail.com>
2023-04-22 01:09:53 +0300

    add redux

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,8 @@ import ReactDOM from 'react-dom/client'
 import { App } from './App.jsx'
 import { BrowserRouter } from 'react-router-dom'
 import { createGlobalStyle } from 'styled-components'
+import { persistor } from './redux/index.js'
+import { PersistGate } from 'redux-persist/integration/react'
 
 const Global = createGlobalStyle`
 	*{
@@ -15,7 +17,9 @@ const Global = createGlobalStyle`
 
 ReactDOM.createRoot(document.getElementById('root')).render(
 	<BrowserRouter>
-		<Global />
-		<App />
+		<PersistGate loading={null} persistor={persistor}>
+			<Global />
+			<App />
+		</PersistGate>
 	</BrowserRouter>
 )
