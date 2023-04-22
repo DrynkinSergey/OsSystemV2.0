c2e92c069679cd73efa40014fe2da56294665ece
Sergey <sergsmile29@gmail.com>
2023-04-22 03:40:53 +0300

    add builder

--- src/redux/userSlice.js
+++ src/redux/userSlice.js
@@ -19,25 +19,35 @@ const userSlice = createSlice({
 			state.users.splice(changedUser, 1, payload)
 		},
 	},
-	extraReducers: {
-		[createUserThunk.pending](state) {
-			state.loading = true
-		},
-		[createUserThunk.rejected](state, { payload }) {
-			state.loading = false
-			state.error = payload
-		},
-		[createUserThunk.fulfilled](state, { payload }) {
-			state.users.push(payload)
-			state.loading = false
-			state.error = null
-		},
-		[updateUserThunk.fulfilled](state, { payload }) {
-			const changedUser = state.users.findIndex(user => user.id === payload.id)
-			state.users.splice(changedUser, 1, payload)
-			state.loading = false
-			state.error = null
-		},
+	extraReducers: builder => {
+		builder
+			.addCase(createUserThunk.fulfilled, (state, { payload }) => {
+				state.users.push(payload)
+				state.loading = false
+				state.error = null
+			})
+			.addCase(updateUserThunk.fulfilled, (state, { payload }) => {
+				const changedUser = state.users.findIndex(
+					user => user.id === payload.id
+				)
+				state.users.splice(changedUser, 1, payload)
+				state.loading = false
+				state.error = null
+			})
+			.addMatcher(
+				action => action.type.endsWith('/pending'),
+				state => {
+					state.loading = true
+					state.error = null
+				}
+			)
+			.addMatcher(
+				action => action.type.endsWith('/rejected'),
+				(state, { payload }) => {
+					state.loading = false
+					state.error = payload
+				}
+			)
 	},
 })
 
